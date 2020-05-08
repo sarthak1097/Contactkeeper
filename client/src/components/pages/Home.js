83ea86204884b9ac8e4ec217e9b37d532254dfaa
Sarthak <sarthak@stepapp.ai>
2020-05-09 00:19:03 +0530

    changes in home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useContext,useEffect}from 'react'
 import Contacts from '../contacts/Contacts'
 import ContactForm from '../contacts/ContactForm'
 import ContactFilter from '../contacts/ContactFilter'
@@ -6,6 +6,11 @@ import AuthContext from '../../context/auth/authContext'
 
 
  const Home = () => {
+     const authContext = useContext(AuthContext)
+     useEffect(() => {
+       authContext.loadUser();
+        // eslint-disable-next-line         
+     }, [])
     return (
         <div className="grid-2">
             <div>
