6b311325244a49eeeee19ac36dd9a57e9ea25682
Sarthak <sarthak@stepapp.ai>
2020-05-09 00:30:42 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -7,11 +8,22 @@ connectDB();
 //init middleware
 app.use(express.json({ extended:false}))
 
-app.get('/',(req,res) => res.json({msg:'welcom to contactkeeper'}) )
+// app.get('/',(req,res) => res.json({msg:'welcom to contactkeeper'}) )
 //define rotes 
 app.use('/api/users',require('./routes/users'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/contacts',require('./routes/contacts'))
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,() => console.log(`server started on port ${PORT}`)
